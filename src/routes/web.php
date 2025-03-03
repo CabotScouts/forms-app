175a6d000daf39971434ba56cb8371d60d4c4a86
Owen Jones <owen@owenjones.net>
2025-03-03 15:11:15 +0000

    we don't need to do approvals now as that's in the new membership system

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\ApprovalsController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\RootController;
 use App\Http\Controllers\UserController;
@@ -12,24 +11,6 @@ Route::controller(AuthController::class)->group(function () {
   Route::get('/login/redirect', 'oauthRedirect')->name('auth.oauth.redirect');
   Route::get('/login/return', 'oauthReturn')->name('auth.oauth.return');
   Route::get('/logout', 'logout')->name('auth.logout');
-});
-
-Route::controller(ApprovalsController::class)->prefix('approvals')->group(function () {
-  // List all notifications requiring approval (with filter to view all)
-  Route::get('/{filter?}', 'list')->name('approvals.list');
-
-  // View an individual NAN and submit approval/comments
-  Route::get('/nan/{id}/view', 'viewNAN')->name('approvals.nan.view');
-  Route::post('/nan/{id}/update', 'updateNAN')->name('approvals.nan.update');
-
-  // View an individual AAN and submit approval/comments
-  Route::get('/aan/{id}/view', 'viewAAN')->name('approvals.aan.view');
-  Route::post('/aan/{id}/update', 'updateAAN')->name('approvals.aan.update');
-
-  // View list of contacts (who receive submitted NANs)
-  Route::get('/contacts', 'viewContacts')->name('approvals.contacts.view');
-  Route::post('/contacts/add', 'addContact')->name('approvals.contacts.add');
-  Route::post('/contacts/update', 'updateContacts')->name('approvals.contacts.update');
 });
 
 Route::controller(RootController::class)->group(function () {
