3349a8025caa12cd98b02dd4127d4a1eb3c38ca1
Owen Jones <owen@owenjones.net>
2025-04-07 15:32:10 +0100

    fake form var passed back, will be for viewing submitted forms in the future

--- src/app/Http/Controllers/RootController.php
+++ src/app/Http/Controllers/RootController.php
@@ -2,17 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class RootController
 {
   public function index()
   {
-    return view('notification');
+    return view('notification', ["form" => false]);
   }
 
   public function submit(Request $request)
   {
+    $validated = $request->validate([
+      'h-captcha-response' => ['hcaptcha'],
+    ]);
+
     return $request->all();
   }
 }
