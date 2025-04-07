d8167f6b7c524080b7957b8b33d3b2758586483e
Owen Jones <owen@owenjones.net>
2025-04-07 15:57:36 +0100

    initial validation rules

--- src/app/Http/Controllers/RootController.php
+++ src/app/Http/Controllers/RootController.php
@@ -15,6 +15,36 @@ class RootController
   public function submit(Request $request)
   {
     $validated = $request->validate([
+      'lic-name' => ['required', 'max:255'],
+      'lic-email' => ['required', 'email'],
+      'lic-phone' => ['required'],
+
+      'submitter-name' => [],
+      'submitter-email' => ['nullable', 'email'],
+
+      'group' => ['required'],
+      'section' => ['required'],
+
+      'number-squirrels' => ['nullable', 'gte:0'],
+      'number-beavers' => ['nullable', 'gte:0'],
+      'number-cubs' => ['nullable', 'gte:0'],
+      'number-scouts' => ['nullable', 'gte:0'],
+      'number-explorers' => ['nullable', 'gte:0'],
+      'number-adults' => ['nullable', 'gte:0'],
+
+      'date' => ['required', 'date'],
+      'location' => ['required'],
+      'description' => ['required'],
+
+      'activity-leader' => [],
+      'activity-leader-email' => ['nullable', 'email'],
+
+      'risk-assessments' => [],
+
+      'intouch' => ['required'],
+
+      'team-leader-email' => ['required', 'email'],
+
       'h-captcha-response' => ['hcaptcha'],
     ]);
 
