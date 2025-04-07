f1422f82151fdffb00a705a97ac51031bd29bea1
Owen Jones <owen@owenjones.net>
2025-04-07 14:35:24 +0100

    add margin to filepond component

--- src/resources/views/components/filepond.blade.php
+++ src/resources/views/components/filepond.blade.php
@@ -1,4 +1,4 @@
-<input type="file" id="{{ $id }}" class="filepond" name="{{ $name }}" multiple data-allow-reorder="true"
+<input type="file" id="{{ $id }}" class="filepond mt-2 mb-2" name="{{ $name }}" multiple data-allow-reorder="true"
   data-max-file-size="3MB" data-max-files="5"
   accept="application/pdf, application/msword, application/vnd.openxmlformats-officedocument.wordprocessingml.document, application/vnd.ms-excel, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
   @required($required ?? false)>
