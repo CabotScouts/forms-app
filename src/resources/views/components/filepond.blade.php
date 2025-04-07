31dd45b3273e1058fc21d80b3776c4c3ebfc3e23
Owen Jones <owen@owenjones.net>
2025-04-07 15:32:43 +0100

    made more readable

--- src/resources/views/components/filepond.blade.php
+++ src/resources/views/components/filepond.blade.php
@@ -1,4 +1,9 @@
-<input type="file" id="{{ $id }}" class="filepond" name="{{ $name }}" multiple data-allow-reorder="true"
+<input 
+  type="file"
+  id="{{ $id }}"
+  class="filepond"
+  name="{{ $name }}"
+  multiple data-allow-reorder="true"
   data-max-file-size="3MB" data-max-files="5"
   accept="application/pdf, application/msword, application/vnd.openxmlformats-officedocument.wordprocessingml.document, application/vnd.ms-excel, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
   @required($required ?? false)>
