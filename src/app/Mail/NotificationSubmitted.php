1e379f4d433aeccc9ed2b6d6741844f0e9ee52e5
Owen Jones <owen@owenjones.net>
2025-04-08 23:58:05 +0100

    first version of email

--- src/app/Mail/NotificationSubmitted.php
+++ src/app/Mail/NotificationSubmitted.php
@@ -9,45 +9,27 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
+use App\Models\Notification;
+
 class NotificationSubmitted extends Mailable
 {
     use Queueable, SerializesModels;
 
-    /**
-     * Create a new message instance.
-     */
-    public function __construct()
-    {
-        //
-    }
+    public function __construct(
+        public Notification $notification
+    ) {}
 
-    /**
-     * Get the message envelope.
-     */
     public function envelope(): Envelope
     {
         return new Envelope(
-            subject: 'Notification Submitted',
+            subject: 'Activity Notification Submitted',
         );
     }
 
-    /**
-     * Get the message content definition.
-     */
     public function content(): Content
     {
         return new Content(
             markdown: 'mail.notification.submitted',
         );
-    }
-
-    /**
-     * Get the attachments for the message.
-     *
-     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
-     */
-    public function attachments(): array
-    {
-        return [];
     }
 }
--- src/resources/views/mail/notification/submitted.blade.php
+++ src/resources/views/mail/notification/submitted.blade.php
@@ -1,11 +1,52 @@
 <x-mail::message>
-# Introduction
+# Activity Notification
 
-The body of your message.
+@if($notification->submitter_name)
+Submitted on {{ $notification->created_at }} by {{ $notification->submitter_name }} ({{ $notification->submitter_email }})
+@else
+Submitted on {{ $notification->created_at }} by {{ $notification->lic_name }} ({{ $notification->lic_email }})
+@endif
 
-<x-mail::button :url="''">
-Button Text
-</x-mail::button>
+## Details
+{{ $notification->description }}
+
+<x-mail::table>
+| Activity Information |                                 |
+| :------------------- | :------------------------------ |
+| Date                 | {{ $notification->date }}       |
+| Leader in Charge     | {{ $notification->lic_name }}   |
+| Email                | {{ $notification->lic_email }}  |
+| Phone                | {{ $notification->lic_phone }}  |
+</x-mail::table>
+
+## Location
+{{ $notification->location }}
+
+<x-mail::table>
+| Group & Section         |                                                        |
+| :---------------------- | :----------------------------------------------------- |
+| Group                   | {{ $notification->group }}                             |
+| Section                 | {{ $notification->section }}                           |
+@foreach(['squirrels', 'beavers', 'cubs', 'scouts', 'explorers', 'adults'] as $section)
+@if($notification->getAttribute('number_'.$section))
+| {{ ucfirst($section) }} | {{ $notification->getAttribute('number_'.$section) }}  |
+@endif
+@endforeach
+| Lead Email              | {{ $notification->team_leader_email }}                 |
+</x-mail::table>
+
+@if($notification->activity_leader)
+## Activity Leader/Permit Holder
+{{ $notification->activity_leader }}
+@endif
+
+## inTouch Arrangements
+{{ $notification->intouch }}
+
+## Risk Assessments
+@foreach($notification->uploads()->get() as $upload)
+* <x-mail::link :url="$upload->url()">{{ $upload->name }}</x-mail::link>
+@endforeach
 
 Thanks,<br>
 {{ config('app.name') }}
