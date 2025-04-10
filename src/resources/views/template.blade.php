a31964ef3c3f5aa6db27b65779ef5e0a05af3a7f
Owen Jones <owen@owenjones.net>
2025-04-10 15:53:54 +0100

    added an icon

--- src/resources/views/template.blade.php
+++ src/resources/views/template.blade.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-
+  <link rel="icon" href="{{ asset('static/icon.png') }}">
   <link rel="stylesheet" href="{{ asset('static/scoutstrap.min.css') }}">
   <link
     href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,400;1,600&display=swap"
