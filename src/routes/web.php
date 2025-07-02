1f48f631d9089cd4234e320ec4baf05590cdb2dc
Owen Jones <owen@owenjones.net>
2025-07-02 01:17:14 +0100

    debug routes only available if we're in debug mode

--- src/routes/web.php
+++ src/routes/web.php
@@ -23,8 +23,11 @@ Route::controller(RootController::class)->group(function () {
 Route::controller(ActivityNotificationController::class)->prefix('/activity-notification')->group(function () {
   Route::get('/', 'index')->name('notification.form');
   Route::post('/submit', 'submit')->name('notification.submit');
-  Route::get('/view/{id}', 'view');
-  Route::get('/resend/{id}', 'resend');
+  
+  if(config('app.debug')) {
+    Route::get('/view/{id}', 'view');
+    Route::get('/resend/{id}', 'resend');
+  }
 });
 
 
@@ -33,6 +36,9 @@ Route::controller(FirstAidController::class)->prefix('/first-aid')->group(functi
   Route::post('/internal/submit', 'submitInternal')->name('fa.internal.submit');
   Route::get('/external', 'external')->name('fa.external.form');
   Route::post('/external/submit', 'submitExternal')->name('fa.external.submit');
-  Route::get('/view/{id}', 'view');
-  Route::get('/resend/{id}', 'resend');
+
+  if(config('app.debug')) {
+    Route::get('/view/{id}', 'view');
+    Route::get('/resend/{id}', 'resend');
+  }
 });
