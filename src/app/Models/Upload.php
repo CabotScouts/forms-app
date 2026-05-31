b9119b2dc5559d1678dd3fdf1267b320a7784063
Owen Jones <owen@owenjones.net>
2026-05-31 23:18:41 +0100

    Tidy up where we do the processing of uploads

--- src/app/Models/Upload.php
+++ src/app/Models/Upload.php
@@ -4,7 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Storage;
+
+use App\Exceptions\FileUploadError;
 
 class Upload extends Model
 {
@@ -29,6 +32,33 @@ class Upload extends Model
     {
         if(Storage::exists($this->basePath())) {
             Storage::delete($this->basePath());
+        }
+    }
+
+    static public function processUploads($submitted)
+    {
+        try {
+            $submitted = json_decode($submitted);
+            $filepond = app(\Sopamo\LaravelFilepond\Filepond::class);
+            $uploads = [];
+
+            foreach($submitted as $sid) {
+                $temppath = $filepond->getPathFromServerId($sid);
+                if(Storage::exists($temppath)) {
+                    $file = basename($temppath);
+                    $uuid = Str::uuid()->toString();
+                    $path = sprintf("uploads/%s_%s", $uuid, $file);
+                    $fullpath = sprintf("public/%s", $path);
+                    Storage::move($temppath, $fullpath);
+                    $uploads[] = Upload::create(['name' => $file, 'path' => $path]);
+                } else {
+                    throw new FileUploadError("Temporary file not found");
+                }
+            }
+            
+            return $uploads;
+        } catch (Exception) {
+            throw new FileUploadError("Unknown file upload error");
         }
     }
 }
